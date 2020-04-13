03b3b785e3e02ed518084d0fac7bfbbb32de6fb6
Sofiane MOKHTARI <35307774+sofiane-mokhtari@users.noreply.github.com>
2020-04-13 21:18:53 +0200

    [sofiane] add discriminant calcul

--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"regexp"
 	"strconv"
+	"math"
 	"strings"
 )
 
@@ -57,7 +58,6 @@ func casting_one(input string) (Cast, bool) {
 	r := regexp.MustCompile(`((\-\s):?)?(\d+(\.\d+)?)`)
 	value :=  conv_value(r.FindString(input))
 	tmp := Strstr(input, "^")
-
 	if (strings.Contains(tmp, "-")) {
 		fmt.Println("Degre cannot be negative")
 		err = true
@@ -76,7 +76,7 @@ func reduce(input []Cast, to_add Cast) []Cast {
 	new_tab := make([]Cast, 0 ,1)
 	for _, i := range input {
 		if (i.degre == to_add.degre) {
-			i.value = i.value + to_add.value
+			i.value = i.value - to_add.value
 			have_not_been_add = false
 		}
 		new_tab = append(new_tab, i)
@@ -110,7 +110,7 @@ func casting_reduce(input []string) ([]Cast, bool) {
 	return tab, false
 }
 
-func print_polynomial_degre(equa []Cast) bool {
+func print_polynomial_degre(equa []Cast) int {
 	higher_degre := 0
 	for _, i := range equa {
 		if (i.degre > higher_degre) {
@@ -118,7 +118,7 @@ func print_polynomial_degre(equa []Cast) bool {
 		}
 	}
 	fmt.Println("Polynomial degree:", higher_degre)
-	return higher_degre < 3
+	return higher_degre
 }
 
 func print_equation(to_print []Cast) {
@@ -140,6 +140,36 @@ func print_equation(to_print []Cast) {
 	fmt.Println(" = 0")
 }
 
+func get_for_degre(equa []Cast, degre int) float64 {
+	for _, i := range equa {
+		if (i.degre == degre) {
+			return i.value
+		}
+	}
+	return 0
+}
+
+func discriminate(equa []Cast) {
+	a := get_for_degre(equa, 2)
+	b := get_for_degre(equa, 1)
+	c := get_for_degre(equa, 0)
+	delta := b * b - 4 * a * c
+	if (delta > 0) {
+		delta_sqrt := math.Sqrt(delta)
+		x_1 := (b * -1 + delta_sqrt) / (2 * a)
+		x_2 := (b * -1 - delta_sqrt) / (2 * a)
+		fmt.Println("Discriminant is strictly positive, the two solutions are :")
+		fmt.Println(x_1)
+		fmt.Println(x_2)
+	} else if (delta == 0) {
+		x := (b * -1) / (2 * a)
+		fmt.Println("Discriminant is equal to zero, the only solution is :")
+		fmt.Println(x)
+	} else {
+		fmt.Println("Their is no solution")
+	}
+}
+
 func main() {
 	if (len(os.Args) < 2) {
 		fmt.Println("Missing arg")
@@ -150,8 +180,11 @@ func main() {
 	tmp, err := casting_reduce(matches)
 	if err == false { 
 		print_equation(tmp)
-		if (!print_polynomial_degre(tmp)) {
+		higher_degre := print_polynomial_degre(tmp)
+		if (higher_degre > 2) {
 			fmt.Println("The polynomial degree is stricly greater than 2, I can't solve.")
+		} else if higher_degre == 2 {
+			discriminate(tmp)
 		}
 	}
 }
