e61a8b29c39ae4ea70f335fa6cec4ea0809bb5a1
sofiane-mokhtari <sofianemokht@gamil.com>
2020-10-21 16:36:13 +0200

    basique test

--- src/tests/basic_test.go
+++ src/tests/basic_test.go
@@ -6,7 +6,7 @@
 /*   By: smokhtar <smokhtar@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2020/09/23 12:48:29 by smokhtar          #+#    #+#             */
-/*   Updated: 2020/10/21 13:51:13 by smokhtar         ###   ########.fr       */
+/*   Updated: 2020/10/21 15:22:16 by smokhtar         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,11 +15,49 @@ package tests
 import (
     "fmt"
     "source"
+    "strconv"
     "testing"
 )
 
-func TestParsing(t *testing.T) {
+var (
+    Black   = Color("\033[1;30m%s\033[0m")
+    Red     = Color("\033[1;31m%s\033[0m")
+    Green   = Color("\033[1;32m%s\033[0m")
+    Yellow  = Color("\033[1;33m%s\033[0m")
+    Purple  = Color("\033[1;34m%s\033[0m")
+    Magenta = Color("\033[1;35m%s\033[0m")
+    Teal    = Color("\033[1;36m%s\033[0m")
+    White   = Color("\033[1;37m%s\033[0m")
+  )
 
+func Color(colorString string) func(...interface{}) string {
+    sprint := func(args ...interface{}) string {
+        return fmt.Sprintf(colorString,
+        fmt.Sprint(args...))
+    }
+    return sprint
+}
+
+func castToString(input []source.Cast) string {
+    if (len(input) == 0) {
+        return "[]"
+    }
+    str := "["
+    for i, e := range (input) {
+        if (i >= 1) {
+            str = str + " "
+        }
+        str = str + fmt.Sprintf("%v", e)
+    }
+    str = str + "]"
+    return str
+}
+
+func TestComputerV1(t *testing.T) {
+    doTestParsing()
+}
+
+func doTestParsing() {
     inputs := [...]string{
         "",
         "2X^2 + 1 = 1",
@@ -27,57 +65,19 @@ func TestParsing(t *testing.T) {
         "5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0",
     }
 
-	for index, i := range inputs {
-        tmp, err := source.Parsing(i)
-        fmt.Println("\t test", index , tmp, err, "Degre : " , source.Get_polynomial_degre(tmp))
+    expected := [...]string{
+        "[]",
+        "[]d",
+        "[{1 0} {4 1}]",
+        "[{4 0} {4 1} {-9.3 2}]",
     }
 
+    for i, e := range inputs {
+        tmp, err := source.Parsing(e)
+        if (expected[i] != castToString(tmp)) {
+            fmt.Println(Red("--- FAIL:\t\tTestParsing Number " + strconv.Itoa(i) + " ", err))
+        } else {
+            fmt.Println(Green("--- SUCCES:\t\tTestParsing Number " + strconv.Itoa(i) + " "))
+        }
+    }
 }
-
-// func TestDegre(t *testing.T) {
-
-//     inputs := [...]string{
-//         "",
-//         "2X^2 + 1 = 1",
-//         "5 * X^0 + 4 * X^1 = 4 * X^0",
-//         "5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0",
-//     }
-
-// 	for index, i := range inputs {
-//         tmp, err := source.Parsing(i)
-//         fmt.Println("\t test", index , tmp, err)
-//     }
-
-// }
-
-// func TestReduce(t *testing.T) {
-
-//     inputs := [...]string{
-//         "",
-//         "2X^2 + 1 = 1",
-//         "5 * X^0 + 4 * X^1 = 4 * X^0",
-//         "5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0",
-//     }
-
-// 	for index, i := range inputs {
-//         tmp, err := source.Parsing(i)
-//         fmt.Println("\t test", index , tmp, err)
-//     }
-
-// }
-
-// func TestResult(t *testing.T) {
-
-//     inputs := [...]string{
-//         "",
-//         "2X^2 + 1 = 1",
-//         "5 * X^0 + 4 * X^1 = 4 * X^0",
-//         "5 * X^0 + 4 * X^1 - 9.3 * X^2 = 1 * X^0",
-//     }
-
-// 	for index, i := range inputs {
-//         tmp, err := source.Parsing(i)
-//         fmt.Println("\t test", index , tmp, err)
-//     }
-
-// }
